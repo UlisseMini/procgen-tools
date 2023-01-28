0a5b0d593e879a79c244e92f925a579ea79eee91
Alex Turner <turneral1@grinnell.edu>
2023-01-28 15:13:22 -0800

    Refactored seed-running code to allow easy configuration of what gets patched and what coeffs to run. Also fixed bug in Uli's code which spawned an extra mouse sprite.

--- experiments/rollout-patch.py
+++ experiments/rollout-patch.py
@@ -1,5 +1,7 @@
 # %%
 # Imports
+from typing import List, Tuple, Dict, Union, Optional, Callable
+
 import numpy as np
 import pandas as pd
 import torch as t
@@ -36,6 +38,7 @@ try:
 except NameError:
     in_jupyter = False
 
+rand_region = 5
 path_prefix = '../' if in_jupyter else ''
 
 def create_venv(num: int, start_level: int = 0, num_levels: int = 1):
@@ -58,12 +61,12 @@ def copy_venv(venv, idx: int):
 def venv_patch_pair(seed: int):
     "Return a venv of 2 environments from a seed, one with cheese, one without cheese"
     venv = create_venv(num=2, start_level=seed)
-    state_bytes_list = venv.env.callmethod("get_state")[1]
+    state_bytes_list = venv.env.callmethod("get_state")
     state = maze.EnvState(state_bytes_list[1])
 
     # TODO(uli): The multiple sources of truth here suck. Ideally one object linked to venv auto-updates(?)
     grid = state.full_grid()
-    grid[grid == maze.CHEESE] = 0
+    grid[grid == maze.CHEESE] = maze.EMPTY
     state.set_grid(grid)
     state_bytes_list[1] = state.state_bytes
     venv.env.callmethod("set_state", state_bytes_list)
@@ -82,11 +85,14 @@ def load_venv_from_file(path: str):
     venv.step = _step
     return venv
 
+# %%
 # Load model
-policy = models.load_policy(path_prefix + 'trained_models/maze_I/model_rand_region_15.pth', 15,
+policy = models.load_policy(path_prefix + f'trained_models/maze_I/model_rand_region_{rand_region}.pth', 15,
     t.device('cpu'))
 
-
+def forward_func_policy(network, inp):
+    hidden = network.embedder(inp)
+    return network.fc_policy(hidden)
 
 
 # %% 
@@ -114,7 +120,7 @@ def logits_to_action_plot(logits, title=''):
     # Get px imshow of the logits, with the action labels, showing the title
 
 # Get patching function 
-def patch_layer(hook, values, coeff:float, activation_label: str, venv, levelpath: str = '', display_bl: bool = True):
+def patch_layer(hook, values, coeff:float, activation_label: str, venv, levelpath: str = '', display_bl: bool = True, vanished=False):
     """
     Subtract coeff*(values[0, ...] - values[1, ...]) from the activations at label given by activation_label.  If display_bl is True, plot using logits_to_action_plot and video of rollout in the first environment specified by venv. Saves movie at "videos/lvl-{seed}-{coeff}.mp4".
     """
@@ -129,7 +135,7 @@ def patch_layer(hook, values, coeff:float, activation_label: str, venv, levelpat
     patches = {activation_label: lambda outp: outp - coeff*cheese_diff}
 
     mode = 'patched' if coeff != 0 else 'unpatched'
-    env = copy_venv(venv, 0)
+    env = copy_venv(venv, 1 if vanished else 0)
     with hook.use_patches(patches):
         seq, _, _ = cro.run_rollout(predict, env, max_steps=250, deterministic=False)
 
@@ -139,40 +145,48 @@ def patch_layer(hook, values, coeff:float, activation_label: str, venv, levelpat
     if display_bl:
         logits_to_action_plot(action_logits, title=activation_label)
         
-        vidpath = path_prefix + f'videos/lvl-{levelpath}-{coeff}.mp4'
+        vidpath = path_prefix + f'videos/{rand_region}/lvl-{levelpath}-{coeff}.mp4'
         clip = ImageSequenceClip([aa.to_numpy() for aa in seq.renders], fps=10.)
         clip.write_videofile(vidpath, logger=None)
         display(Video(vidpath, embed=True))
 
+
+# %%
 label = 'embedder.block2.res1.resadd_out'
-diff_coeffs = (0, 1, 2, 3, 4, 5, 10, 20, 50, 100, 1000)
+diff_coeffs = [0, 1, 2, 3, 4, 5, 10, 20, 50, 100, 1000]
+hook = cmh.ModuleHook(policy)
+
+# %% 
+# Infrastructure for running different kinds of seeds
+def run_seed(seed:int, hook: cmh.ModuleHook, diff_coeffs: List[float], display_bl: bool = True, values:Optional[np.ndarray]=None, label='embedder.block2.res1.resadd_out'):
+    """ Run a single seed, with the given hook, diff_coeffs, and display_bl. If values is provided, use those values for the patching. Otherwise, generate them via a cheese/no-cheese activation diff.""" 
+    venv = venv_patch_pair(seed) 
+
+    # Get values if not provided
+    if values is None:
+        obs = venv.reset().astype(np.float32)
+        hook.probe_with_input(obs, func=forward_func_policy)
+        values = hook.get_value_by_label(label)
+
+    # Show behavior on the level without cheese
+    patch_layer(hook, values, 0, label, venv, levelpath=f'vanished-{seed}', display_bl=display_bl, vanished=True)
+
+    for coeff in diff_coeffs:
+        # hook.probe_with_input(obs, func=forward_func_policy) # TODO does this have to be reset?
+        patch_layer(hook, values, coeff, label, venv, levelpath=seed, display_bl=display_bl, vanished=False)
+
+        # Wait for input from jupyter notebook
+        # print(f"Finished {seed} {diff_coeff}")
+        # if in_jupyter:
+        #     input("Press Enter to continue...")
 
 # %%
 # Sweep all levels using patches gained from each level
-hook = cmh.ModuleHook(policy)
-def forward_func_policy(network, inp):
-    hidden = network.embedder(inp)
-    return network.fc_policy(hidden)
-
-for diff_coeff in diff_coeffs:
-    for seed in range(num):
-        venv = load_venv_from_file('mazes/lvl-num-'+seed+'.pkl')
-        obs = venv.reset().astype(np.float32)
-
-        hook.probe_with_input(obs, func=forward_func_policy)
-        values = hook.get_value_by_label(label)
-        patch_layer(hook, values, diff_coeff, label, venv, levelpath=seed, display_bl=True)
-        # Wait for input from jupyter notebook
-        print(f"Finished {seed} {diff_coeff}")
-        if in_jupyter:
-            input("Press Enter to continue...")
+for seed in range(50):
+    run_seed(seed, hook, diff_coeffs)
 
 # %% 
 # Try using one patch for many levels at different strengths
-hook = cmh.ModuleHook(policy)
-def forward_func_policy(network, inp):
-    hidden = network.embedder(inp)
-    return network.fc_policy(hidden)
 
 fixed_value_source = '0-rev'
 venv = load_venv_from_file(f'mazes/lvl-num-{fixed_value_source}.pkl')
@@ -181,19 +195,13 @@ obs = venv.reset().astype(np.float32)
 hook.probe_with_input(obs, func=forward_func_policy)
 values = hook.get_value_by_label(label)
 
-for diff_coeff in (1, 2, 3, 5, 10, 20):
-    for seed in ('0',):  
-        venv = load_venv_from_file(f'mazes/lvl-num-'+seed+'.pkl')
-        # hook.probe_with_input(obs, func=forward_func_policy)
-        patch_layer(hook, values, diff_coeff, label, venv, levelpath=f'{seed}-fixed-{fixed_value_source}')
+for seed in range(20):  
+    run_seed(seed, hook, diff_coeffs, values=values)
 
 # %% 
 # Try all labels 
-labels = list(hook.values_by_label.keys())
+labels = list(hook.values_by_label.keys()) # TODO this dict was changing in size during the loop, but why?
 for label in labels: # block2 res2 resadoutt seems promising somehow?
-    # if label == 'embedder.block1.maxpool_out': break 
-    values = hook.get_value_by_label(label)
-    patch_layer(hook, values, label, venv)
-    hook.probe_with_input(obs, func=forward_func_policy)
+    run_seed(0, hook, diff_coeffs, label=label)
 
 # %%
