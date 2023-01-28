cbd1af760d64d9897c0544893a59cf2fad0707cd
Alex Turner <turneral1@grinnell.edu>
2023-01-27 17:11:28 -0800

    Refactored rollout-patch

--- experiments/rollout-patch.py
+++ experiments/rollout-patch.py
@@ -87,14 +87,6 @@ hook.probe_with_input(obs, func=forward_func_policy)
 # print(hook.values_by_label.keys())
 
 # %% 
-# Get patching function 
-activation_label = 'embedder.block3.res2.resadd_out'
-value = hook.get_value_by_label(activation_label)
-cheese_diff = value[0,...] - value[1,...] # Add this to activation_label's activations during forward passes
-# mask = t.from_numpy(np.ones(1,dtype=bool))
-
-patches = {activation_label: lambda outp: outp - cheese_diff}
-
 # Custom predict function to match rollout expected interface, uses
 # the hooked network so it is patchable
 def predict(obs, deterministic):
@@ -106,16 +98,7 @@ def predict(obs, deterministic):
         act = dist.sample().numpy() # Sample from distribution
     return act, None
 
-
-# %% 
-# Reload a single env; can't do two at once
-
-env = copy_venv(venv, 0)
-# use env, a copy of the first venv env
-
-# %%
-# Make a video of the renders so we can see what it looks like
-def logits_to_action_plot(logits):
+def logits_to_action_plot(logits, title=''):
     """
     Plot the action logits as a heatmap, ignoring bogus repeat actions. Use px.imshow. Assumes logits is a DataArray of shape (n_steps, n_actions).
     """
@@ -124,37 +107,39 @@ def logits_to_action_plot(logits):
     action_indices = models.MAZE_ACTION_INDICES
     prob_dict = models.human_readable_actions(t.distributions.categorical.Categorical(probs=prob))
     prob_dist = t.stack(list(prob_dict.values()))
-    px.imshow(prob_dist, y=[k.title() for k in prob_dict.keys()]).show()
+    px.imshow(prob_dist, y=[k.title() for k in prob_dict.keys()],title=title).show()
+    # Get px imshow of the logits, with the action labels, showing the title
 
-DETERMINISTIC = False
-MAX_STEPS = 100
-# Run a normal, unpatched roll-out
-seq, _, _ = cro.run_rollout(predict, env, max_steps=MAX_STEPS, deterministic=DETERMINISTIC)
+# Get patching function 
+def patch_layer(hook, activation_label: str, venv):
+    """
+    Add the cheese-diff patch to the activations of the given layer. Plot using logits_to_action_plot and video of rollout. 
+    """
+    print(f'=====Patching layer {activation_label}=====')
+    value = hook.get_value_by_label(activation_label)
+    cheese_diff = value[0,...] - value[1,...] # Add this to activation_label's activations during forward passes
 
-# Probe to get all the activations for this rollout, then show the action logits
-hook.probe_with_input(seq.obs.astype(np.float32))
-action_logits_label = 'fc_policy_out'
-action_logits_orig = hook.get_value_by_label(action_logits_label)
-logits_to_action_plot(action_logits_orig)
+    patches = {activation_label: lambda outp: outp - cheese_diff}
 
-# Make a video of the renders so we can see what it looks like
-vid_fn, fps = cro.make_video_from_renders(seq.renders, fps=10)
-display(Video(vid_fn, embed=True))
+    DETERMINISTIC = False
+    MAX_STEPS = 100
+    action_logits_label = 'fc_policy_out'
 
-# %% 
-# Run a patched rollout
-env = copy_venv(venv, 0) 
+    for mode in ('original', 'patched'):
+        env = copy_venv(venv, 0)
+        if mode == 'patched':
+            with hook.patch(patches):
+                seq, _, _ = cro.run_rollout(predict, env, max_steps=MAX_STEPS, deterministic=DETERMINISTIC)
+        else:
+            seq, _, _ = cro.run_rollout(predict, env, max_steps=MAX_STEPS, deterministic=DETERMINISTIC)
 
-with hook.use_patches(patches):
-    seq_patched, _, _ = cro.run_rollout(predict, env, max_steps=MAX_STEPS, deterministic=DETERMINISTIC)
-# Probe to get all the activations for this rollout, then show the action logits
-hook.probe_with_input(seq_patched.obs.astype(np.float32), patches=patches)
-action_logits_patched = hook.get_value_by_label(action_logits_label)
-logits_to_action_plot(action_logits_patched)
-
-
-vid_fn, fps = cro.make_video_from_renders(seq_patched.renders, fps=10)
-display(Video(vid_fn, embed=True))
-# Do px.imshow, but with the action names as the x-axis
+        hook.probe_with_input(seq.obs.astype(np.float32))
+        action_logits = hook.get_value_by_label(action_logits_label)
+        logits_to_action_plot(action_logits, title=f'Original logits for env {i}')
+        vid_fn, fps = cro.make_video_from_renders(seq.renders, fps=10)
+        display(Video(vid_fn, embed=True))
 
 # %%
+# Try all labels 
+for label in hook.values_by_label.keys():
+    patch_layer(hook, label, venv)
