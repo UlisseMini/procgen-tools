fea16e75526135b4cae601fece0eb41b9c6a04e8
Ulisse Mini <ulisse.mini@gmail.com>
2023-02-03 13:44:40 -0800

    Final-ish tweaks to chessvec notebook

--- experiments/vfield_stats.py
+++ experiments/vfield_stats.py
@@ -69,3 +69,24 @@ def plot_decision_probs(decision_probs_original, decision_probs_patched):
         a.set_title(f'{label} probability')
 
     return fig
+
+
+
+def plot_vfs(vfs):
+    fig, ax = plt.subplots(1,2, figsize=(10,5))
+    for a in ax:
+        a.set_xticks([])
+        a.set_yticks([])
+
+    for i, vf in enumerate((vfs['original_vfield'], vfs['patched_vfield'])):
+        legal_mouse_positions, arrows, grid = vf['legal_mouse_positions'], vf['arrows'], vf['grid']
+
+        ax[i].set_xlabel("Original vfield" if i == 0 else "Patched vfield")
+        ax[i].quiver(
+            [x[1] for x in legal_mouse_positions], [x[0] for x in legal_mouse_positions],
+            [x[1] for x in arrows], [x[0] for x in arrows], color='red',
+        )
+        ax[i].imshow(grid, origin='lower', dpi=300)
+
+    plt.title(f"Seed {vfs['seed']}, coeff {vfs['coeff']}")
+    return fig
