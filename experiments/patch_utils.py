d5820be6bede3c1c534f72451bf9bcf120d5f7d1
Alex Turner <turneral1@grinnell.edu>
2023-02-06 10:36:19 -0800

    Use HBox to display custom maze pair

--- experiments/patch_utils.py
+++ experiments/patch_utils.py
@@ -7,7 +7,7 @@ import torch as t
 import plotly.express as px
 import matplotlib.pyplot as plt
 from IPython.display import Video, display, clear_output
-from ipywidgets import Text, interact
+from ipywidgets import Text, interact, HBox
 from moviepy.video.io.ImageSequenceClip import ImageSequenceClip
 
 # NOTE: this is Monte's RL hooking code (and other stuff will be added in the future)
@@ -53,7 +53,7 @@ def get_cheese_venv_pair(seed: int):
 def get_custom_venv_pair(seed: int, num_envs=2):
     """ Allow the user to edit num_envs levels from a seed. Return a venv containing both environments. """
     venv = create_venv(num=num_envs, start_level=seed, num_levels=1)
-    display(maze.venv_editor(venv, check_on_dist=False))
+    display(HBox(maze.venv_editor(venv, check_on_dist=False)))
     return venv
 
 def load_venv_pair(path: str):
