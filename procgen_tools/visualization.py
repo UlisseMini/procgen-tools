233ff4a9ecc6ebab47ea242af8215eca2c0af00a
Alex Turner <turneral1@grinnell.edu>
2023-02-15 15:17:50 -0800

    Refactored ActivationsPlotter into visualization.py

--- procgen_tools/visualization.py
+++ procgen_tools/visualization.py
@@ -124,3 +124,109 @@ def custom_vfield(policy : t.nn.Module, venv : ProcgenGym3Env = None, seed : int
     widget_vbox = Box(children=editors + [output], layout=Layout(display='flex', flex_flow='row', align_items='stretch', width='100%'))
 
     return widget_vbox
+
+class ActivationsPlotter:
+    def __init__(self, labels: List[str], plotter: Callable, activ_gen: Callable, hook, coords_enabled: bool=False, defaults : dict = None, **act_kwargs):
+        """
+        labels: The labels of the layers to plot
+        plotter: A function that takes a label, channel, and activations and plots them
+        activ_gen: A function that takes a label and obs and returns the activations which should be sent to plotter 
+        hook: The hook that contains the activations
+        coords_enabled: Whether to enable the row and column sliders
+        defaults: A dictionary of default values for the plotter, where the keys are attributes of this class which themselves have the "value" attribute. The class value will be set to the corresponding dictionary value.
+        act_kwargs: Keyword arguments to pass to the activations generator
+        """
+        self.fig = go.FigureWidget()
+
+        self.plotter = plotter
+        self.activ_gen = activ_gen
+        self.act_kwargs = act_kwargs
+        self.hook = hook
+
+        # Remove the _out layer and "embedder." prefixes
+        formatted_labels = format_labels(labels)
+        self.label_widget = Dropdown(options=formatted_labels, value=formatted_labels[0], description="Layers")
+        self.channel_slider = IntSlider(min=0, max=127, step=1, value=0, description="Channel")
+        self.widgets = [self.fig, self.label_widget, self.channel_slider]
+
+        self.coords_enabled = coords_enabled
+        if coords_enabled:
+            self.col_slider, self.row_slider = (IntSlider(min=0, max=62, step=1, value=32, description="Column"), IntSlider(min=0, max=63, step=1, value=32, description="Row"))
+            self.widgets.extend([self.col_slider, self.row_slider])
+
+        self.filename_widget = Text(value="", placeholder="Custom filename", disabled=False)
+        self.filename_widget.layout.width = '150px'
+
+        self.button = Button(description="Save image")
+        self.button.on_click(self.save_image)
+        self.widgets.append(HBox([self.filename_widget, self.button]))
+
+        if defaults is not None:
+            for key, value in defaults.items():
+                getattr(self, key).value = value
+
+        for widget in self.widgets:
+            if widget != self.fig:
+                widget.observe(self.update_plotter, names='value')
+        self.update_plotter()
+
+    def display(self):
+        """ Display the elements; this function separates functionality from implementation. """
+        display(self.fig)
+        display(VBox(self.widgets[1:-1])) # Show a VBox of the label dropdown and the sliders, centered beneath the plot
+        display(self.widgets[-1])
+
+    def save_image(self, b): # Add a save button to save the image
+        basename = self.filename_widget.value if self.filename_widget.value != "" else f"{self.label_widget.value}_{self.channel_slider.value}{f'_{self.col_slider.value}_{self.row_slider.value}' if self.coords_enabled else ''}"
+        filepath = f"{PATH_PREFIX}experiments/visualizations/{basename}.png"
+
+        # Annotate to the outside of the plot
+        old_title = self.fig.layout.title
+        self.fig.layout.title = f"{self.label_widget.value};\nchannel {self.channel_slider.value}{f' at ({self.col_slider.value}, {self.row_slider.value})' if self.coords_enabled else ''}"
+
+        self.fig.write_image(filepath)
+        print(f"Saved image to {filepath}")
+
+        self.fig.layout.title = old_title # Clear the title
+        
+        self.filename_widget.value = "" # Clear the filename_widget box
+
+    def update_plotter(self, b=None):
+        """ Update the plot with the current values of the widgets. """
+        label = expand_label(self.label_widget.value)
+        if self.coords_enabled:
+            col, row = self.col_slider.value, self.row_slider.value
+            activations = self.activ_gen(row, col, label, **self.act_kwargs)
+        else:
+            activations = self.activ_gen(label, **self.act_kwargs) # shape is (b, c, h, w) at conv layers, (b, activations) at linear layers
+
+        shap = self.hook.get_value_by_label(label).shape
+        self.channel_slider.max = shap[1] - 1 if len(shap) > 2 else 0
+        self.channel_slider.value = min(self.channel_slider.value, self.channel_slider.max)
+        channel = self.channel_slider.value
+
+        if len(activations.shape) == 2: # Linear layer (batch, hidden_dim) TODO check this 
+            # Unsqueeze the np.ndarray
+            activations = np.expand_dims(activations, axis=(1,2))
+            # If there's only a single channel, display a 1D Heatmap, with a single rowvalue and the activation indices as the col values TODO remove columns
+        else: 
+            assert channel < activations.shape[1], "Channel doesn't exist at this layer"
+
+        self.fig.update_layout(height=500, width=500, title_text=self.label_widget.value)
+        if label == 'fc_policy_out':
+            # Transform each index into the corresponding action label, according to maze.py 
+            self.fig.update_xaxes(ticktext=[models.human_readable_action(i).title() for i in range(NUM_ACTIONS)], tickvals=np.arange(activations.shape[3])) # TODO is this correct, for 3 instead of 2?
+        else: # Reset the indices so that there are no xticks
+            self.fig.update_xaxes(ticktext=[], tickvals=[])
+
+        self.fig.update_xaxes(side="top") # Set the x ticks to the top
+        self.fig.update_yaxes(autorange="reversed") # Reverse the row-axis autorange
+        
+        self.plotter(activations=activations[:, channel], fig=self.fig) # Plot the activations
+
+        # Set the min and max to be the min and max of all channels at this label
+        bounds = np.abs(activations).max()
+        self.fig.update_traces(zmin=-1 * bounds, zmid=0, zmax=bounds)    
+        
+        # Change the colorscale to split red (negative) -- white (zero) -- blue (positive)
+        self.fig.update_traces(colorscale='RdBu')
