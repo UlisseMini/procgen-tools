10415d26ef44b902b3fbaac08ed6dcbd7cbed764
Alex Turner <turneral1@grinnell.edu>
2023-02-20 10:31:05 -0800

    Added helper function for making a save button

--- playground/reverse_c55.py
+++ playground/reverse_c55.py
@@ -16,25 +16,49 @@ setup() # create directory structure and download data
 from procgen_tools.imports import *
 from procgen_tools import patch_utils, visualization, vfield, maze
 
+def create_button(prefix : str, fig : plt.Figure, descriptors : defaultdict[str, float]): # TODO put in visualization.py?
+    """ Create a button that saves fig to a file. 
+    
+    Args:
+        prefix (str): The prefix of the filename.
+        fig (plt.Figure): The figure to save.
+        descriptors (defaultdict[str, float]): A dictionary of descriptors to add to the filename.
+    """
+    def save_fig(b):
+        """ Save the figure to a file. """
+        filename = f'{prefix}_'
+        for key, value in descriptors.items():
+            # Replace any dots with underscores
+            value = str(value).replace('.', '_')
+            filename += f'{key}_{value}_'
+        filename = filename[:-1] + '.png' # remove trailing underscore
+        fig.savefig(filename)
+        # Display the filename
+        display(Markdown(f'Figure saved to `{filename}`'))
+
+    button = Button(description='Save figure')
+    button.on_click(save_fig)
+    return button
+
+save_dir = 'playground/visualizations'
+AX_SIZE = 6
+
 # %% Doubling c55
 @interact
 def double_channel_55(seed=IntSlider(min=0, max=100, step=1, value=0), multiplier=FloatSlider(min=-15, max=15, step=0.1, value=5.5)):
     venv = patch_utils.get_cheese_venv_pair(seed=seed)
     patches = patch_utils.get_multiply_patch(layer_name=default_layer, channel=55, multiplier=multiplier)
-    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=6)
+    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=AX_SIZE)
     plt.show()
 
-    def save_fig(b):
-        fig.savefig(f'visualizations/c55_multiplier_{multiplier}_seed_{seed}.png')
-    button = Button(description='Save figure')
-    button.on_click(save_fig)
+    button = create_button(prefix=f'{save_dir}/c55_multiplier', fig=fig, descriptors=defaultdict[str, float](seed=seed, multiplier=multiplier))
     display(button)
 
 # %% [markdown]
 # Let's see whether the c55 synthetic patch reproduces behavior in the unpatched model. 
 
 # %%
-# For each seed, compute the cheese location and then find an appropriate channel patch
+# For each seed, compute the cheese location and then synthesize a patch that assigns positive activation to the corresponding location in the c55 channel
 @interact 
 def find_cheese(seed=IntSlider(min=0, max=100, step=1, value=0), value=FloatSlider(min=-15, max=15, step=0.1, value=.9)):
     venv = patch_utils.get_cheese_venv_pair(seed=seed)
@@ -43,35 +67,29 @@ def find_cheese(seed=IntSlider(min=0, max=100, step=1, value=0), value=FloatSlid
     chan_row, chan_col = visualization.get_channel_from_grid_pos((cheese_row, cheese_col), layer=default_layer)
     patches = patch_utils.get_channel_pixel_patch(layer_name=default_layer, channel=55, coord=(chan_row, chan_col), value=value) 
 
-    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=6)
+    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=AX_SIZE)
     visualization.plot_pixel_dot(axs[1], row=chan_row, col=chan_col)
 
     fig.suptitle(f'Channel 55, position {chan_row}, {chan_col}')
     
     plt.show()
 
-    def save_fig(b): # TODO make this a library call
-        fig.savefig(f'playground/visualizations/c55_synthetic_seed_{seed}.png')
-    button = Button(description='Save figure')
-    button.on_click(save_fig)
+    button = create_button(prefix=f'{save_dir}/c55_pixel_synthetic', fig=fig, descriptors=defaultdict[str, float](seed=seed, value=value))
     display(button)
 
 # %% Compare with patching a different channel with the same synthetic patch
 @interact
-def c55_patch_transfer_channels(seed=IntSlider(min=0, max=100, step=1, value=0), channel=IntSlider(min=0, max=63, step=1, value=54)):
+def c55_patch_transfer_across_channels(seed=IntSlider(min=0, max=100, step=1, value=0), channel=IntSlider(min=0, max=63, step=1, value=54)):
     venv = patch_utils.get_cheese_venv_pair(seed=seed)
     cheese_pos = maze.get_cheese_pos_from_seed(seed)
     channel_pos = visualization.get_channel_from_grid_pos(cheese_pos, layer=default_layer)
     patches = patch_utils.get_channel_pixel_patch(layer_name=default_layer, channel=channel, coord=channel_pos)
 
-    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=6)
+    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=AX_SIZE)
     fig.suptitle(f'Channel {channel}, position {channel_pos}')
     plt.show()
 
-    def save_fig(b):
-        fig.savefig(f'playground/visualizations/c{channel}_synthetic_seed_{seed}.png')
-    button = Button(description='Save figure')
-    button.on_click(save_fig)
+    button = create_button(prefix=f'{save_dir}/c55_synthetic_cheese_transfer', fig=fig, descriptors=defaultdict[str, float](seed=seed, channel=channel))
     display(button)
 
 # %% Random patching channels
@@ -84,13 +102,24 @@ def random_channel_patch(seed=IntSlider(min=0, max=100, step=1, value=0), layer_
 
     venv = patch_utils.get_cheese_venv_pair(seed=seed)
     patches = patch_utils.get_random_patch(layer_name=layer_name, hook=hook, channel=channel)
-    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=6)
+    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=AX_SIZE)
     plt.show()
 
-    def save_fig(b):
-        fig.savefig(f'playground/visualizations/c55_random_seed_{seed}.png')
-    button = Button(description='Save figure')
-    button.on_click(save_fig)
+    button = create_button(prefix=f'{save_dir}/random_channel_patch', fig=fig, descriptors=defaultdict[str, float](seed=seed, layer_name=layer_name, channel=channel))
     display(button)
 
-# %%
+# %% Causal scrub 55
+# We want to replace the channel 55 activations with the activations from a randomly generated maze with cheese at the same location
+GENERATE_NUM = 1 
+@interact
+def causal_scrub_55(target=IntSlider(min=0, max=100, step=1, value=0)):
+    venv = patch_utils.get_cheese_venv_pair(seed=target)
+    # Get the cheese location from the target seed
+    cheese_row, cheese_col = maze.get_cheese_pos_from_seed(target)
+    # Generate another seed with cheese at the same location, moving cheese to the appropriate locations 
+    seeds, grids = maze.generate_mazes_with_cheese_at_location((cheese_row, cheese_col), num_mazes = GENERATE_NUM, skip_seed=source_seed)
+    patches = patch_utils.get_causal_scrub_patch(layer_name=default_layer, channel=55, seed=target)
+    fig, axs, info = patch_utils.compare_patched_vfields(venv, patches, hook, render_padding=True, ax_size=AX_SIZE)
+    plt.show()
+
+    button = create_button(prefix=f'{save_dir}/c55_causal_scrub', fig=fig, descriptors=defaultdict[str, float](source=source, target=target))
