ba6714d4af9c713a43b3d1f98e0e130b62258bb2
Alex Turner <turneral1@grinnell.edu>
2023-03-03 16:53:54 -0800

    Added random maze generation for post

--- playground/presentation.py
+++ playground/presentation.py
@@ -112,3 +112,7 @@ for seed in (0, 20, 60):
     save_sweep(channels=cheese_channels, seed=seed, value=.9, channels_str='cheese_channels')
 
 # %%
+
+# Randomly generate 5 mazes and save a GIF for each
+for seed in np.random.randint(0, 100000, 5):
+    save_sweep(channels=cheese_channels, seed=seed, value=.9, channels_str='cheese_channels')
