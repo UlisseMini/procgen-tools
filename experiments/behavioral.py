3f910febfc6ff71df7ecdf14de33cc82f5ed4bf0
Alex Turner <turneral1@grinnell.edu>
2023-03-03 16:01:23 -0800

    Added max_size rejection sampling to generate_plots

--- experiments/behavioral.py
+++ experiments/behavioral.py
@@ -24,14 +24,21 @@ cols = 2
 # Control show_components with a checkbox
 checkbox = widgets.Checkbox(value=False, description='Show action components')
 
-def generate_plots():
-    """ Generate plots for random seeds. """
+def generate_plots(max_size : int = 3):
+    """ Generate plots for random seeds with inner grid size at most max_size. """
     # Ensure no double-shown plots
-    plt.close('all')
+    assert max_size >= 3, f"The smallest mazes have size 3; {max_size} is too small!"
 
+    plt.close('all')
     fig, axs = plt.subplots(rows, cols, figsize=(AX_SIZE*cols, AX_SIZE*rows))
     for idx, ax in enumerate(axs.flatten()): 
         seed = np.random.randint(0, 100000)
+        while maze.get_inner_grid_from_seed(seed=seed).shape[0] > max_size:
+            print(f"Seed {seed} has inner grid size {maze.get_inner_grid_from_seed(seed=seed).shape[0]}; skipping...")
+            seed = np.random.randint(0, 100000)
+            print(seed)
+        print(maze.get_inner_grid_from_seed(seed=seed).shape[0])
+        print(seed)
         venv = maze.create_venv(num=1, start_level=seed, num_levels=0)
         vf = vfield.vector_field(venv, policy=hook.network)
         vfield.plot_vf(vf, ax=ax, show_components=checkbox.value)
