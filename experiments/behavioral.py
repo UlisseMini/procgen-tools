76cbabb371b5bfeb81708df7033494de89079d54
Alex Turner <turneral1@grinnell.edu>
2023-03-03 16:26:03 -0800

    Fixed multiple rendering of seeds in behavioral.py

--- experiments/behavioral.py
+++ experiments/behavioral.py
@@ -22,12 +22,23 @@ AX_SIZE = 4
 # Control show_components with a checkbox
 checkbox = widgets.Checkbox(value=False, description='Show action components')
 
+rows = 1
+cols = 2
+
+fig_out = widgets.Output()
+text_out = widgets.Output()
+display(text_out)
+display(fig_out)
+
 def generate_plots(max_size : int = 18, cols : int = 2, rows : int = 1):
     """ Generate rows*cols plots for random seeds with inner grid size at most max_size. """
     # Ensure no double-shown plots
     assert max_size >= 3, f"The smallest mazes have size 3; {max_size} is too small!"
 
-    plt.close('all')
+    # Indicate that the plots are being generated    
+    with text_out:
+        print(f'Generating {rows*cols} plots with max inner grid size {max_size}...')
+
     fig, axs = plt.subplots(rows, cols, figsize=(AX_SIZE*cols, AX_SIZE*rows))
     for idx, ax in enumerate(axs.flatten()): 
         seed = np.random.randint(0, 100000)
@@ -37,8 +48,13 @@ def generate_plots(max_size : int = 18, cols : int = 2, rows : int = 1):
         vf = vfield.vector_field(venv, policy=hook.network)
         vfield.plot_vf(vf, ax=ax, show_components=checkbox.value)
         ax.set_title(f'Seed: {seed:,}')
-        ax.axis('off')
-    plt.show(fig)
+        ax.axis('off') 
+
+    # Indicate that the plots are done being generated
+    text_out.clear_output()
+    fig_out.clear_output(wait=True)
+    with fig_out:     
+        plt.show()
 
 
 # Make slider for max inner grid size
@@ -51,4 +67,3 @@ button.on_click(lambda _: generate_plots(max_size = slider.value))
 display(HBox([button, checkbox]))
 
 generate_plots(max_size = slider.value)
-# %%
