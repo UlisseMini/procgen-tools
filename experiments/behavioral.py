940df5ebe5efc35bf45b7db2c6d66b53b46802a4
Alex Turner <turneral1@grinnell.edu>
2023-03-03 16:04:46 -0800

    Added max grid size widget to behavioral.py

--- experiments/behavioral.py
+++ experiments/behavioral.py
@@ -18,14 +18,12 @@ from procgen_tools import visualization, patch_utils, maze, vfield
 
 # %% Generate vfields for randomly generated seeds
 AX_SIZE = 4
-rows = 1
-cols = 2
 
 # Control show_components with a checkbox
 checkbox = widgets.Checkbox(value=False, description='Show action components')
 
-def generate_plots(max_size : int = 3):
-    """ Generate plots for random seeds with inner grid size at most max_size. """
+def generate_plots(max_size : int = 18, cols : int = 2, rows : int = 1):
+    """ Generate rows*cols plots for random seeds with inner grid size at most max_size. """
     # Ensure no double-shown plots
     assert max_size >= 3, f"The smallest mazes have size 3; {max_size} is too small!"
 
@@ -34,22 +32,23 @@ def generate_plots(max_size : int = 3):
     for idx, ax in enumerate(axs.flatten()): 
         seed = np.random.randint(0, 100000)
         while maze.get_inner_grid_from_seed(seed=seed).shape[0] > max_size:
-            print(f"Seed {seed} has inner grid size {maze.get_inner_grid_from_seed(seed=seed).shape[0]}; skipping...")
             seed = np.random.randint(0, 100000)
-            print(seed)
-        print(maze.get_inner_grid_from_seed(seed=seed).shape[0])
-        print(seed)
-        venv = maze.create_venv(num=1, start_level=seed, num_levels=0)
+        venv = maze.create_venv(num=1, start_level=seed, num_levels=1)
         vf = vfield.vector_field(venv, policy=hook.network)
         vfield.plot_vf(vf, ax=ax, show_components=checkbox.value)
         ax.set_title(f'Seed: {seed:,}')
         ax.axis('off')
     plt.show(fig)
 
+
+# Make slider for max inner grid size
+slider = widgets.IntSlider(min=3, max=25, step=1, value=18, description='Max grid size')
+display(slider)
+
 # Make a button to generate new plots
 button = widgets.Button(description='Generate new plots')
-button.on_click(lambda _: generate_plots())
+button.on_click(lambda _: generate_plots(max_size = slider.value))
 display(HBox([button, checkbox]))
 
-generate_plots()
+generate_plots(max_size = slider.value)
 # %%
