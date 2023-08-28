58f891f2ca33ff07d7c5da3218bcc5a4a9ffdce2
Alex Turner <turneral1@grinnell.edu>
2023-08-28 11:37:08 -0700

    Updated statistics gather_data to put data in directories

--- experiments/statistics/gather_data.py
+++ experiments/statistics/gather_data.py
@@ -1,35 +1,70 @@
-from procgen import ProcgenGym3Env
+import os
 import torch
-from procgen_tools import maze
-from procgen_tools.models import load_policy
 from tqdm import tqdm
 import numpy as np
 import pickle
 from argparse import ArgumentParser
-import random
+
+from procgen import ProcgenGym3Env
 from procgen_tools.data_utils import Episode
+from procgen_tools import maze
+from procgen_tools.models import load_policy
 
 
-def create_venv(num_levels = 1, start_level = 0):
+def create_venv(num_levels=1, start_level=0):
     venv = ProcgenGym3Env(
         num=1,
-        env_name='maze', num_levels=num_levels, start_level=start_level,
-        distribution_mode='hard', num_threads=4, render_mode="rgb_array",
+        env_name="maze",
+        num_levels=num_levels,
+        start_level=start_level,
+        distribution_mode="hard",
+        num_threads=4,
+        render_mode="rgb_array",
     )
     venv = maze.wrap_venv(venv)
     return venv
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     parser = ArgumentParser()
-    parser.add_argument('--model_file', type=str, default='./trained_models/maze_I/model_rand_region_5.pth')
-    parser.add_argument('--num_timesteps', type=int, default=256, help='maximum timesteps per episode')
-    parser.add_argument('--num_episodes', type=int, default=10000, help='number of episodes to collect (agent finishes or times out)')
-    parser.add_argument('--argmax', action='store_true', help='argmax logits instead of sampling. often gets stuck, but when successful has less jittering')
+    parser.add_argument(
+        "--model_file",
+        type=str,
+        default="./trained_models/maze_I/model_rand_region_5.pth",
+    )
+    parser.add_argument(
+        "--num_timesteps",
+        type=int,
+        default=256,
+        help="maximum timesteps per episode",
+    )
+    parser.add_argument(
+        "--num_episodes",
+        type=int,
+        default=10000,
+        help="number of episodes to collect (agent finishes or times out)",
+    )
+    parser.add_argument(
+        "--argmax",
+        action="store_true",
+        help=(
+            "argmax logits instead of sampling. often gets stuck, but when"
+            " successful has less jittering"
+        ),
+    )
 
     args = parser.parse_args()
 
-    policy = load_policy(args.model_file, action_size=15, device=torch.device('cpu'))
-    model_name = args.model_file.split('/')[-1][:-4]
+    policy = load_policy(
+        args.model_file, action_size=15, device=torch.device("cpu")
+    )
+    model_name = args.model_file.split("/")[-1][:-4]
+
+    # Create a directory based on model_name if it doesn't exist
+    save_dir = os.path.join("experiments/statistics/data", model_name)
+    os.makedirs(
+        save_dir, exist_ok=True
+    )  # This creates the directory if it doesn't already exist
 
     # determinism
     max_seed = 1000000
@@ -39,10 +74,10 @@ if __name__ == '__main__':
     for seed in tqdm(seeds):
         seed = int(seed)
         venv = create_venv(start_level=seed)
-        assert venv.num_envs == 1, 'Only one env supported (for now)'
+        assert venv.num_envs == 1, "Only one env supported (for now)"
 
         # grab initial_state_bytes and initial info for episode object
-        states_bytes = venv.env.callmethod('get_state')[0]
+        states_bytes = venv.env.callmethod("get_state")[0]
         states_vals = maze._parse_maze_state_bytes(states_bytes)
         info = venv.env.get_info()
 
@@ -53,7 +88,9 @@ if __name__ == '__main__':
         episode = Episode(
             initial_state_bytes=states_bytes,
             mouse_positions_outer=mouse_positions_outer,
-            actions=actions, rewards=rewards, sampler=sampler,
+            actions=actions,
+            rewards=rewards,
+            sampler=sampler,
             level_seed=seed,
         )
 
@@ -71,15 +108,18 @@ if __name__ == '__main__':
                 # IMPORTANT: we don't update episode here. otherwise we'll log the last frame (a new level)
                 break
 
-            states_bytes = venv.env.callmethod('get_state')[0]
+            states_bytes = venv.env.callmethod("get_state")[0]
             states_vals = maze._parse_maze_state_bytes(states_bytes)
 
             rewards.append(float(rew[0]))
             actions.append(int(act[0]))
             mouse_positions_outer.append(maze.get_mouse_pos_sv(states_vals))
 
-
-        # get basename of model file
-        present_dir = 'experiments/statistics'
-        with open(f'{present_dir}/data/{model_name}-seed{seed}-{sampler}-{episode.steps}steps.pkl', 'wb') as f: # TODO: Compression, batch trajectories
+        # Change the path for saving data
+        with open(
+            os.path.join(
+                save_dir, f"seed{seed}-{sampler}-{episode.steps}steps.pkl"
+            ),
+            "wb",
+        ) as f:
             pickle.dump(episode, f, protocol=pickle.HIGHEST_PROTOCOL)
